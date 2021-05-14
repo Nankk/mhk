d3060c741043ed56211e0fbb4fed2b4cd60b5013
Nankk Home <toc32768@yahoo.co.jp>
2021-05-14 21:16:52 +0900

    MetaとかNONCONVERT（無変換）とか対応してねぇ

--- src/mhk/listener.clj
+++ src/mhk/listener.clj
@@ -1,6 +1,8 @@
 (ns mhk.listener
-  (:import [com.tulskiy.keymaster.common Provider HotKeyListener]
-           [javax.swing KeyStroke]))
+  (:require [mhk.robot :as robot])
+  (:import [com.tulskiy.keymaster.common HotKeyListener Provider]
+           java.awt.event.KeyEvent
+           javax.swing.KeyStroke))
 
 (defonce provider (atom nil))
 
@@ -12,14 +14,44 @@
   (reify HotKeyListener
     (onHotKey [this hotKey] (apply f (take (get-arity f) [hotKey])))))
 
-(defn register [^String key-code f]
+(defmulti register (fn [keycode-or-stroke f] (type keycode-or-stroke)))
+
+(defmethod register java.lang.String
+  [keycode f]
   (when (nil? @provider)
     (reset! provider (Provider/getCurrentProvider true)))
-  (let [^Provider p @provider]
-    (.register p (KeyStroke/getKeyStroke key-code) (make-listener f))))
+  (if-let [keystroke (KeyStroke/getKeyStroke keycode)]
+    (let [^Provider p @provider]
+      (.register p keystroke (make-listener f)))
+    (throw (Exception. (str "Invalid keycode " keycode)))))
+
+(defmethod register javax.swing.KeyStroke
+  [keystroke f]
+  (when (nil? @provider)
+    (reset! provider (Provider/getCurrentProvider true)))
+  (if keystroke
+    (let [^Provider p @provider]
+      (.register p keystroke (make-listener f)))
+    (throw (Exception. "Invalid keystroke nil."))))
 
 (defn reset []
   (when-let [^Provider p @provider]
     (.reset p)
     (.stop p)
     (reset! provider nil)))
+
+(comment
+  (reset)
+  (register (KeyStroke/getKeyStroke KeyEvent/VK_META 0) #(println "無変換 pressed."))
+  (register "alt shift I" #(do
+                             (robot/key-release KeyEvent/VK_ALT)
+                             (robot/key-release KeyEvent/VK_SHIFT)
+                             (robot/key-type KeyEvent/VK_UP)
+                             (robot/key-press KeyEvent/VK_ALT)
+                             (robot/key-press KeyEvent/VK_SHIFT)
+                             ))
+  (register "shift alt I" #(println "shift alt i pressed."))
+  (KeyStroke/getKeyStroke  "SHIFT I")
+  (KeyStroke/getKeyStroke  "META")
+  (KeyStroke/getKeyStroke KeyEvent/VK_NONCONVERT 0)
+  (.register p (KeyStroke/getKeyStroke key-code) (make-listener f)))
--- src/mhk/robot.clj
+++ src/mhk/robot.clj
@@ -3,8 +3,19 @@
            java.awt.event.KeyEvent
            javax.swing.KeyStroke))
 
-(defonce robot (atom nil))
+(defonce robot (Robot.))
 
 (defn key-press [^KeyEvent key-event]
-  (when-not @robot
-    (reset! robot ())))
+  (when key-event
+    (.keyPress robot key-event)))
+
+(defn key-release [^KeyEvent key-event]
+  (when key-event
+    (.keyRelease robot key-event)))
+
+(defn key-type
+  "Press and release the key."
+  [^KeyEvent key-event]
+  (when key-event
+    (.keyPress robot key-event)
+    (.keyRelease robot key-event)))
